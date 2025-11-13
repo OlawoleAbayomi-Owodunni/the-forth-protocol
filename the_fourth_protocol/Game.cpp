7b3d40c1d553aad0314838c193237c46f3ee1dc5
WollyTech <ayolawole@gmail.com>
2025-11-13 04:50:46 +0000

    Project Initialised

---
+++ the_fourth_protocol/Game.cpp
@@ -0,0 +1,129 @@
+#include "Game.h"
+#include <iostream>
+
+// Our target FPS
+static double const FPS{ 60.0f };
+
+////////////////////////////////////////////////////////////
+Game::Game()
+	: m_window(sf::VideoMode(sf::Vector2u(ScreenSize::s_width, ScreenSize::s_height), 32), "SFML Playground", sf::Style::Default)
+{
+	init();
+}
+
+////////////////////////////////////////////////////////////
+void Game::init()
+{
+	// Really only necessary is our target FPS is greater than 60.
+	m_window.setVerticalSyncEnabled(true);
+	
+	if (!m_arialFont.openFromFile("ASSETS/FONTS/ariblk.ttf"))
+	{
+		std::cout << "Error loading font file";
+	}
+
+#ifdef TEST_FPS
+	x_updateFPS.setFont(m_arialFont);
+	x_updateFPS.setPosition(sf::Vector2f(20, 300));
+	x_updateFPS.setCharacterSize(24);
+	x_updateFPS.setFillColor(sf::Color::White);
+	x_drawFPS.setFont(m_arialFont);
+	x_drawFPS.setPosition(sf::Vector2f(20, 350));
+	x_drawFPS.setCharacterSize(24);
+	x_drawFPS.setFillColor(sf::Color::White);
+#endif
+}
+
+////////////////////////////////////////////////////////////
+void Game::run()
+{
+	sf::Clock clock;
+	sf::Time timeSinceLastUpdate = sf::Time::Zero;
+	
+	sf::Time timePerFrame = sf::seconds(1.0f / FPS); // 60 fps
+	while (m_window.isOpen())
+	{
+		processEvents(); // as many as possible
+		timeSinceLastUpdate += clock.restart();
+		while (timeSinceLastUpdate > timePerFrame)
+		{
+			timeSinceLastUpdate -= timePerFrame;
+			processEvents(); // at least 60 fps
+			update(timePerFrame.asMilliseconds()); //60 fps
+#ifdef TEST_FPS
+			x_secondTime += timePerFrame;
+			x_updateFrameCount++;
+			if (x_secondTime.asSeconds() > 1)
+			{
+				std::string updatesPS = "UPS " + std::to_string(x_updateFrameCount - 1);
+				x_updateFPS.setString(updatesPS);
+				std::string drawsPS = "DPS " + std::to_string(x_drawFrameCount);
+				x_drawFPS.setString(drawsPS);
+				x_updateFrameCount = 0;
+				x_drawFrameCount = 0;
+				x_secondTime = sf::Time::Zero;
+			}
+#endif
+		}
+		render(); // as many as possible
+#ifdef TEST_FPS
+		x_drawFrameCount++;
+#endif
+	}
+}
+
+////////////////////////////////////////////////////////////
+void Game::processEvents()
+{
+    while (const std::optional event = m_window.pollEvent())
+    {
+        if (event->is<sf::Event::Closed>())
+        {
+            m_window.close();
+        }
+        processGameEvents(*event);
+    }
+}
+
+
+////////////////////////////////////////////////////////////
+void Game::processGameEvents(const sf::Event& event)
+{
+	if (const auto* keyPressed = event.getIf<sf::Event::KeyPressed>())
+	{
+		switch (keyPressed->scancode)
+		{
+		case sf::Keyboard::Scancode::Escape:
+			m_window.close();
+			break;
+		case sf::Keyboard::Scancode::Up:
+			// Up key was pressed...
+			break;
+		default:
+			break;
+		}
+	}
+}
+
+
+////////////////////////////////////////////////////////////
+void Game::update(double dt)
+{
+
+}
+
+////////////////////////////////////////////////////////////
+void Game::render()
+{
+	m_window.clear(sf::Color(0, 0, 0, 0));
+#ifdef TEST_FPS
+	m_window.draw(x_updateFPS); //ups is 60 and dps is 61
+	m_window.draw(x_drawFPS);
+#endif
+	m_window.display();
+}
+
+
+
+
+
---
+++ the_fourth_protocol/Game.h
@@ -0,0 +1,104 @@
+#pragma once
+
+// If VS Debug build is enabled, then any block of code enclosed within 
+//  the preprocessor directive #ifdef TEST_FPS is compiled and executed.
+#ifdef _DEBUG
+#define TEST_FPS
+#endif // _DEBUG
+
+#include <SFML/Graphics.hpp>
+#include <string>
+
+using namespace std;
+using namespace sf;
+
+/// <summary>
+/// @author OA-O
+/// @date November 2025
+/// @version 1.0
+/// 
+/// </summary>
+
+/// <summary>
+/// @brief Main class for the SFML Playground project.
+/// 
+/// This will be a single class framework for learning about SFML. 
+/// Example usage:
+///		Game game;
+///		game.run();
+/// </summary>
+
+struct ScreenSize
+{
+public:
+	static const int s_width{ 1440 };
+
+	static const int s_height{ 900 };
+};
+
+class Game
+{
+public:
+	/// <summary>
+	/// @brief Default constructor that initialises the SFML window, 
+	///   and sets vertical sync enabled. 
+	/// </summary>
+	Game();
+
+	/// <summary>
+	/// @brief the main game loop.
+	/// 
+	/// A complete loop involves processing SFML events, updating and drawing all game objects.
+	/// The actual elapsed time for a single game loop is calculated. If this value is 
+	///  greater than the target time for one loop (1 / 60), then (and only then) is an update 
+	///  operation performed.
+	/// The target is at least one update and one render cycle per game loop, but typically 
+	///  more render than update operations will be performed as we expect our game loop to
+	///  complete in less than the target time.
+	/// </summary>
+	void run();
+
+protected:
+	/// <summary>
+	/// @brief Once-off game initialisation code
+	/// </summary>	
+	void init();
+	/// <summary>
+	/// @brief Placeholder to perform updates to all game objects.
+	/// </summary>
+	/// <param name="time">update delta time</param>
+	void update(double dt);
+
+	/// <summary>
+	/// @brief Draws the background and foreground game objects in the SFML window.
+	/// The render window is always cleared to black before anything is drawn.
+	/// </summary>
+	void render();
+
+	/// <summary>
+	/// @brief Checks for events.
+	/// Allows window to function and exit. 
+	/// Events are passed on to the Game::processGameEvents() method.
+	/// </summary>	
+	void processEvents();
+
+	/// <summary>
+	/// @brief Handles all user input.
+	/// </summary>
+	/// <param name="event">system event</param>
+	void processGameEvents(const sf::Event&);
+
+	// Font used for all text
+	sf::Font m_arialFont{ "ASSETS/FONTS/ariblk.ttf" };
+	// main window
+	sf::RenderWindow m_window;
+
+#ifdef TEST_FPS
+	sf::Text x_updateFPS{ m_arialFont };	// text used to display updates per second.
+	sf::Text x_drawFPS{  m_arialFont };	// text used to display draw calls per second.
+	sf::Time x_secondTime{ sf::Time::Zero };			// counter used to establish when a second has passed.
+	int x_updateFrameCount{ 0 };						// updates per second counter.
+	int x_drawFrameCount{ 0 };							// draws per second counter.
+#endif // TEST_FPS
+
+};
